c26eea448052d5e3132de7230e7af8d0001ba65c
tiagomestreteixeira <tiago.mestre.teixeira@polarising.com>
2016-08-15 18:09:07 +0100

    Added Lecture Visual Studio Project Files

---
+++ Week2/Lectures/Project Files/gettingStartedMongoDBDriver/gettingStartedMongoDBDriver/Program.cs
@@ -0,0 +1,88 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gettingStartedMongoDBDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            BsonClassMap.RegisterClassMap<Person>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapMember(x => x.Name).SetElementName("name");
+            });
+
+
+            /* Lesson 1 */
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase("test");
+            var col = db.GetCollection<BsonDocument>("people");
+            /* Lesson 2 */
+            var doc = new BsonDocument
+            {
+                { "name","Jones" }
+            };
+            doc.Add("age", 30);
+            doc["profession"] = "hacker";
+            
+            var nestedArray = new BsonArray();
+            nestedArray.Add(new BsonDocument("color", "red"));
+            doc.Add("array", nestedArray);
+
+            Console.WriteLine(doc["array"][0]["color"]);
+
+            /* Lesson 3 */
+            var person = new Person
+            {
+                Name = "Jones",
+                Age = 30,
+                Colors = new List<string> { "red", "blue" },
+                Pets = new List<Pet> { new Pet { Name = "Fluffy", Type = "Pig" } },
+                ExtraElements = new BsonDocument("anotherName", "anotherValue")
+            };
+
+            using (var writer = new JsonWriter(Console.Out))
+            {
+                BsonSerializer.Serialize(writer, person);
+            }
+            Console.WriteLine(doc);
+        }
+
+        /* In Lesson 3 - POCO Representation */
+        class Person
+        {
+            public ObjectId Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public List<string> Colors { get; set; }
+            public List<Pet> Pets { get; set; }
+            public BsonDocument ExtraElements { get; set; }
+        }
+        
+        class Pet
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+        }
+    }
+}
